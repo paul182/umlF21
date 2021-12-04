88afa51df6ccada27ce45241ec03dd263faddc2f
praveen <paul182@gmail.com>
2021-12-04 12:26:54 -0500

    update extract to use s3

--- extract_mysql_full.py
+++ extract_mysql_full.py
@@ -36,3 +36,27 @@ fp.close()
 m_cursor.close()
 conn.close()
 
+# load the aws_boto_credentials values
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get(
+    "aws_boto_credentials",
+    "access_key")
+secret_key = parser.get(
+    "aws_boto_credentials",
+    "secret_key")
+bucket_name = parser.get(
+    "aws_boto_credentials",
+    "bucket_name")
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key)
+
+s3_file = local_filename
+
+s3.upload_file(
+    local_filename,
+    bucket_name,
+    s3_file)
