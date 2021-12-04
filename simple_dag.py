509fd663be31dc77ea5c408e5100a13e13473cab
praveen <paul182@gmail.com>
2021-12-04 10:42:38 -0500

    my work to date

---
+++ simple_dag.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator \
+    import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1)
+}
+
+dag = DAG(
+    'simple_dag',
+    description='A simple DAG',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1)
+)
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id='sleep',
+    depends_on_past=True,
+    bash_command='sleep 3',
+    dag=dag,
+)
+
+t3 = BashOperator(
+    task_id='print_end',
+    start_date = days_ago(1),
+    depends_on_past=True,
+    bash_command='echo \'end\'',
+    dag=dag,
+)
+
+t1 >> t2
+t2 >> t3
